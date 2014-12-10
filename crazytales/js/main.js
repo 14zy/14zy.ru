616039fe7454f755881dc162f54a1151e50dfab2
14zy <just14zy@gmail.com>
2014-12-10 17:35:17 +0300

    рассказы

--- crazytales/js/main.js
+++ crazytales/js/main.js
@@ -1,1 +1,11 @@
+$( "#StartTale" ).click(function() {
+  $('#writer')[0].style.display="block";
+  $('#StartTale')[0].className += " active";
+  $("html, body").animate({ scrollTop: 150 }, "slow");
+});
+
+$( "#ContinueTale" ).click(function() {
+  alert('Ашаля техтенбех расказама')
+});
+
 
