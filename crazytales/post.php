d89ef4ca1edd6addd285ad1a2f7b96d81f7dd181
14zy <just14zy@gmail.com>
2014-12-21 23:17:32 +0300

    Последний на районе

--- crazytales/post.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $name = $_POST['name'];
-  $tale = $_POST['tale'];
-  $fp = fopen("formdata.txt", "a");
-  $savestring = "Имя: " . $name . "\nРассказ: " . $tale . "\n\n";
-  fwrite($fp, $savestring);
-  fclose($fp);
-?>
